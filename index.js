59a0ed47d79dd3a35d92b8e47357a68e6adabf97
Mduduzi <mbongeni.maikesto@gmail.com>
2020-04-15 17:08:48 +0200

    added function to merge two pdf documents

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const { PDFDocument } = require('pdf-lib');
 
 const fs = require('fs');
 
-run().catch(err => {
+/*createPDF().catch(err => {
+    console.log(err);
+});*/
+
+mergePDFDocs().catch(err => {
     console.log(err);
 });
 
-async function run() {
+async function createPDF() {
     //create a new document and add a new page
 
     const doc = await PDFDocument.create();
@@ -25,4 +29,30 @@ async function run() {
 
     //write the pdf to a file
     fs.writeFileSync('./test.pdf', await doc.save());
+}
+
+async function mergePDFDocs() {
+
+
+    //load pdf documents to be merged
+
+    const doc1 = await PDFDocument.load(fs.readFileSync('./test.pdf'));
+    const doc2 = await PDFDocument.load(fs.readFileSync('./atlassian-git-cheatsheet.pdf'));
+
+    //create a new document
+    const mergedDoc = await PDFDocument.create();
+
+    //Add doc1 to the new merged doc
+    const [coverPage] = await mergedDoc.copyPages(doc1, [0]);
+    mergedDoc.addPage(coverPage);
+
+    //add the the content pages
+    const contentPages = await mergedDoc.copyPages(doc2, doc2.getPageIndices());
+
+    for (const page of contentPages) {
+        mergedDoc.addPage(page);
+    }
+
+    fs.writeFileSync('./test2.pdf', await mergedDoc.save());
+
 }
